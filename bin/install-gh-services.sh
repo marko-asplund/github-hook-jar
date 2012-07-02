835b47e0c9c6caff8739b081b9c503c25775aad3
marko asplund <marko.asplund@gmail.com>
2012-07-02 14:44:05 +0300

    automate github-services + Ruby gem installation & setup

---
+++ bin/install-gh-services.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+git clone git@github.com:marko-asplund/github-services.git
+cd github-services
+
+# unset RVM environment settings
+rvm reset
+
+# install Bundler
+jruby -S gem install bundler
+
+# install required Ruby gems
+jruby -S script/bootstrap
+
+# jruby-openssl needs to be installed manually
+jruby -S gem install -i vendor/gems/jruby/1.8 jruby-openssl
+
+# add jruby-openssl to jruby LOAD_PATH
+(echo; find vendor/gems -regex .*/jruby-openssl.*/lib/shared -type d ) >> .bundle/loadpath
+find vendor/gems -regex .*/bouncy-castle-java.*/lib -type d >> .bundle/loadpath
